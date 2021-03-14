4f2d9fadc49fb7a43f15d4904cdbece5d0fdc172
“hymnsbharat234” <chandrakantyadav814101@gmail.com>
2021-03-14 22:41:29 +0530

    updated

--- config/twilio.js
+++ config/twilio.js
@@ -1,6 +1,6 @@
-const env = require('./environment');
-module.exports = {
-    serviceID: 'VA0c1b7cf128a04995205ffecf604f451d',
-    accountSID: 'AC6928Fc046862d8a4af51e3d7c125a670',
-    authToken: '1a24f7a23715985bdc2e1c06cf7cea53'
-}
+// const env = require('./environment');
+// module.exports = {
+//     serviceID: 'VA0c1b7cf128a04995205ffecf604f451d',
+//     accountSID: 'AC6928Fc046862d8a4af51e3d7c125a670',
+//     authToken: '1a24f7a23715985bdc2e1c06cf7cea53'
+// }
